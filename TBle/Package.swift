9b18d74a6d496a9d6b8bfdf5be689bcc49e254f8
Thinh Nguyen <thinhnguyen12389@gmail.com>
2025-06-28 09:53:43 +0700

    Initialize TBle Swift package structure
    Add initial Swift package files for TBle, including .gitignore, Package.swift, an empty source file, and a basic test file. Sets up the project structure for further development.

---
+++ TBle/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TBle",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)    
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TBle",
+            targets: ["TBle"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TBle"),
+        .testTarget(
+            name: "TBleTests",
+            dependencies: ["TBle"]
+        ),
+    ]
+)
